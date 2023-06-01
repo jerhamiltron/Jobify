5db91bf73ad6002852d6e517a5d8c5c8ae745b6d
jerhamiltron <jerham2010@gmail.com>
2023-05-31 21:26:38 -0400

    set edit job dispatched

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -20,6 +20,7 @@ import {
   CREATE_JOB_ERROR,
   GET_JOBS_BEGIN,
   GET_JOBS_SUCCESS,
+  SET_EDIT_JOB,
 } from './actions';
 
 const token = localStorage.getItem('token');
@@ -216,7 +217,12 @@ const AppProvider = ({ children }) => {
   };
 
   const setEditJob = (id) => {
-    console.log(`set edit job: ${id}`);
+    dispatch({ type: SET_EDIT_JOB, payload: { id } });
+  };
+
+  const editJob = () => {
+    try {
+    } catch (error) {}
   };
 
   const deleteJob = (id) => {
@@ -239,6 +245,7 @@ const AppProvider = ({ children }) => {
         getJobs,
         setEditJob,
         deleteJob,
+        editJob,
       }}
     >
       {children}
--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -20,6 +20,7 @@ const AddJob = () => {
     handleChange,
     clearValues,
     createJob,
+    editJob,
   } = useAppContext();
 
   const handleSubmit = (e) => {
@@ -30,7 +31,7 @@ const AddJob = () => {
       return;
     }
     if (isEditing) {
-      // edit job
+      editJob();
       return;
     }
     createJob();
